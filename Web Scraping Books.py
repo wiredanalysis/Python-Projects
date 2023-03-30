2d2270022eb13757310b0ef7dc8b65001e872e09
Wired Analysis <114444119+wiredanalysis@users.noreply.github.com>
2023-03-30 22:27:21 +0300

    Add files via upload

---
+++ Web Scraping Books.py
@@ -0,0 +1,27 @@
+
+#import libraries
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+books = []
+
+for i in range(1,5):
+  url = f"https://books.toscrape.com/catalogue/page-{i}.html"
+  response = requests.get(url)
+  response = response.content
+  soup = BeautifulSoup(response, 'html.parser')
+  ol = soup.find('ol')
+  articles = ol.find_all('article', class_='product_pod')
+  for article in articles:
+    image = article.find('img')
+    title = image.attrs['alt']
+    starTag = article.find('p')
+    star = starTag['class'][1]
+    price = article.find('p', class_='price_color').text
+    price = float(price[1:])
+    books.append([title, star, price])
+    
+
+df = pd.DataFrame(books, columns=['Title', 'Star Rating', 'Price'])
+df.to_csv('books.csv')
